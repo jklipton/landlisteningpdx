30f656b7d6f5c7c3768f8046d48c7b6729612e17
Jen Lipton <jkl.lipton@gmail.com>
2018-07-16 16:08:27 -0700

    removed header and installed eslint'

--- header.php
+++ header.php
@@ -17,11 +17,8 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'landlistening-pdx' ); ?></a>
-
-	<header id="masthead" class="site-header">
-        header.php
+		<!-- header.php -->
 		<nav class="site-nav">
-		<?php wp_list_pages('title_li='); ?>
+			<?php wp_list_pages('title_li='); ?>
 		</nav>
-	</header>
 	   <div id="content" class="site-content">
