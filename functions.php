020b0100607320dd75fac32a8bcd6095586ff1df
Jen Lipton <jkl.lipton@gmail.com>
2018-07-16 15:50:24 -0700

    redo that

--- functions.php
+++ functions.php
@@ -120,7 +120,7 @@ add_action( 'widgets_init', 'landlistening_pdx_widgets_init' );
  * Enqueue scripts and styles.
  */
 function landlistening_pdx_scripts() {
-	wp_enqueue_style( 'style', 'style.css', null);
+	wp_enqueue_style( 'style', '/style.css', null);
 
 	wp_enqueue_script( 'landlistening-pdx-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
