0fbf5fcfd77f95d7e601850eeb8e886350b3a015
Jen Lipton <jkl.lipton@gmail.com>
2018-07-17 13:03:28 -0700

    debug

--- functions.php
+++ functions.php
@@ -98,6 +98,23 @@ function landlistening_pdx_content_width() {
 }
 add_action( 'after_setup_theme', 'landlistening_pdx_content_width', 0 );
 
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function landlistening_pdx_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', 'landlistening-pdx' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'landlistening-pdx' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'landlistening_pdx_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
