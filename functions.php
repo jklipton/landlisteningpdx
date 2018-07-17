41008a5e0cad581030d97d3de0f3fc16c2bd0b17
Jen Lipton <jkl.lipton@gmail.com>
2018-07-16 18:30:53 -0700

    env did not work

--- functions.php
+++ functions.php
@@ -122,7 +122,7 @@ add_action( 'widgets_init', 'landlistening_pdx_widgets_init' );
 function landlistening_pdx_scripts() {
 	$_my_theme = wp_get_theme();
 
-	wp_enqueue_style( 'style', get_stylesheet_uri(), [], WP_ENV == 'production' ? $_my_theme->get('Version') : date());
+	wp_enqueue_style( 'style', get_stylesheet_uri(), [], date());
 
 	wp_enqueue_script( 'landlistening-pdx-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
