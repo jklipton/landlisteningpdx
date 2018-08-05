50feffb8946081c33671848016802eb7dfb8478e
Jen Lipton <jkl.lipton@gmail.com>
2018-08-05 13:41:39 -0700

    testing

--- template-parts/content.php
+++ template-parts/content.php
@@ -6,14 +6,14 @@
  *
  * @package Landlistening_PDX
  */
-$featured_imgs = get_the_post_thumbnail_url(get_the_ID(),'full'); 
+$featured_img = get_the_post_thumbnail_url(get_the_ID(),'full'); 
 $img_url = $featured_imgs[0];
 ?>
 <script>
     console.log(<?= json_encode(landlistening_pdx_post_thumbnail()); ?>);
 </script>
 content.php
-<section id="post-<?php the_title(); ?>" <?php post_class(); ?>  role="page" style="background: url('<? echo $img_url;  ?>'); ">
+<section id="post-<?php the_title(); ?>" <?php post_class(); ?>  role="page" style="background: url('<? echo $featured_img;  ?>'); ">
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' );?>
 	</header><!-- .entry-header -->
