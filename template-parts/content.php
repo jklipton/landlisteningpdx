918e68701e6805ff2e14014179571519718ca573
Jen Lipton <jkl.lipton@gmail.com>
2018-08-05 13:46:02 -0700

    wtf

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,13 +10,13 @@
 $post_id = get_the_ID();
 $thumb_url_array = wp_get_attachment_image_src($post_id, 'full', true);
 // $featured_imgs = get_the_post_thumbnail_url(get_the_ID(),'full'); 
-$img_url = $thumb_url_array[0];
+$featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); 
 ?>
 <script>
     console.log(<?= json_encode($post); ?>);
 </script>
 content.php
-<section id="post-<?php the_title(); ?>" <?php post_class(); ?>  role="page" style="background: url('<? echo $img_url;  ?>'); ">
+<section id="post-<?php the_title(); ?>" <?php post_class(); ?>  role="page" style="background: url('<? echo $featured_img_url;  ?>'); ">
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' );?>
 	</header><!-- .entry-header -->
