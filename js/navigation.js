236816c5ee0bdc3747583e39c86898ec91f2bc6c
Jen Lipton <jkl.lipton@gmail.com>
2018-08-10 11:10:49 -0700

    test me some good ol jquery

--- js/navigation.js
+++ js/navigation.js
@@ -4,5 +4,10 @@
  * Handles the mobile-menu on mobile devices
  */
 (function() {
-  
+  const mobileMenu = $('#menu-mobile-menu');
+  if(!mobileMenu) return;
+
+  mobileMenu.children('li').hide();
+  const menuButton = $('#menu-mobile-menu').firstChild;
+  menuButton.show();
 })();
