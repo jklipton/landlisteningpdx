7fb5c14a83b81b78128f8a90a225c36655b04b71
Jen Lipton <jkl.lipton@gmail.com>
2018-08-10 12:40:12 -0700

    scope this shit

--- js/navigation.js
+++ js/navigation.js
@@ -4,20 +4,26 @@
  * Handles the mobile-menu on mobile devices
  */
 (function($) {
-  console.log('navigation test running', $);
-  const mobileMenu = $('#menu-mobile-menu');
-  if(!mobileMenu) return;
-
-  const menuArray = mobileMenu.children('li');
-  const menuButton = menuArray.first();
-  menuButton.addClass('mobile-menu-first');
-  menuArray.not('.mobile-menu-first').addClass('mobile-menu-slide');
   
-  toggleMobileMenu();
-  menuButton.click(toggleMobileMenu);
 
-  function toggleMobileMenu() {
-    $('.mobile-menu-slide').slideToggle(1000);
-    menuButton.show();
-  }
+  mobileNavInit();
+  
+  function mobileNavInit() {
+    const mobileMenu = $('#menu-mobile-menu');
+    if(!mobileMenu) return;
+    $('.site-info').hide();
+
+    const menuArray = mobileMenu.children('li');
+    const menuButton = menuArray.first();
+    menuButton.addClass('mobile-menu-first');
+    menuArray.not('.mobile-menu-first').addClass('mobile-menu-slide');
+  
+    toggleMobileMenu();
+    menuButton.click(toggleMobileMenu);
+
+    function toggleMobileMenu() {
+      $('.mobile-menu-slide').slideToggle(1000);
+      menuButton.show();
+    }
+  };
 })(jQuery);
